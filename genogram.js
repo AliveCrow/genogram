26bf01011510bad6ac6738b6e1da7f8c344c8b6c
liuwenbo <136907108@qq.com>
2023-07-09 15:52:12 +0800

    ver4

--- genogram.js
+++ genogram.js
@@ -1,626 +1,108 @@
 export default [
     {
-        "id": 32150,
+        "id": 1,
         "关系": "本人",
         "姓名": "丁万强",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/mail.png",
         "gender": "male",
-        "fid": 32160,
-        "mid": 32161,
+        "详情": "(其他、不在册、非安置)",
+        "fid": 11,
+        "mid": 12,
         "pids": [
-            "32151",
-            "321511"
-        ]
-    },
-    {
-        "id": 32151,
-        "关系": "之妻",
-        "姓名": "张改莲",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 32162,
-        "mid": 32163,
-        "pids": [
-            "32150",
-        ]
-    },
-    {
-        "id": 321511,
-        "关系": "之前妻",
-        "姓名": "张改莲",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32150"
-        ]
-    },
-    {
-        "id": 32152,
-        "关系": "之子",
-        "姓名": "丁凯峰",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/mail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": null
-    },
-    {
-        "id": 32153,
-        "关系": "之儿媳",
-        "姓名": "张娅玲",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32152"
-        ]
-    },
-    {
-        "id": 32154,
-        "关系": "之孙",
-        "姓名": "丁诺轩",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/mail.png",
-        "gender": "male",
-        "fid": 32152,
-        "mid": 32153,
-        "pids": null
-    },
-    {
-        "id": 32160,
-        "关系": "之父",
-        "姓名": "父亲",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/mail.png",
-        "gender": "male",
-        "fid": 32168,
-        "mid": 32169,
-        "pids": [
-            "32161"
-        ]
-    },
-    {
-        "id": 32161,
-        "关系": "之母",
-        "姓名": "母亲",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 321670,
-        "mid": 32171,
-        "pids": [
-            "32160"
-        ]
-    },
-    {
-        "id": 32162,
-        "关系": "之岳父",
-        "姓名": "岳父",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/mail.png",
-        "gender": "male",
-        "fid": 321671,
-        "mid": 32172,
-        "pids": [
-            "32163"
-        ]
-    },
-    {
-        "id": 32163,
-        "关系": "之岳母",
-        "姓名": "岳母",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 321672,
-        "mid": 32173,
-        "pids": [
-            "32162"
-        ]
-    },
-    {
-        "id": 32166,
-        "关系": "之长兄",
-        "姓名": "丁万虎",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/mail.png",
-        "gender": "male",
-        "fid": 32160,
-        "mid": 32161,
-        "pids": [
-            "32167"
-        ]
-    },
-    {
-        "id": 32167,
-        "关系": "之长嫂",
-        "姓名": "王",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32166"
-        ]
-    },
-
-    //
-    {
-        "id": 32168,
-        "关系": "爷爷",
-        "姓名": "爷爷",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32169"
-        ]
-    },
-    {
-        "id": 32169,
-        "关系": "奶奶",
-        "姓名": "奶奶",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32168"
-        ]
-    },
-
-    {
-        "id": 321670,
-        "关系": "外公",
-        "姓名": "外公",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32171"
-        ]
-    },
-    {
-        "id": 32171,
-        "关系": "外婆",
-        "姓名": "外婆",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32170"
-        ]
-    },
-
-    {
-        "id": 321671,
-        "关系": "外公1",
-        "姓名": "外公1",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32172"
-        ]
-    },
-    {
-        "id": 32172,
-        "关系": "外婆2",
-        "姓名": "外婆2",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "321671"
-        ]
-    },
-
-    {
-        "id": 321672,
-        "关系": "外公2",
-        "姓名": "外公2",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "32173"
-        ]
-    },
-    {
-        "id": 32173,
-        "关系": "外婆3",
-        "姓名": "外婆3",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "321672"
-        ]
-    },
-
-    // 舅舅
-    {
-        "id": 1,
-        "关系": "舅舅",
-        "姓名": "舅舅",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32170,
-        "mid": 32171,
-        "pids": [
+            "2",
+            "1111111111"
         ]
     },
     {
         "id": 2,
-        "关系": "舅舅2",
-        "姓名": "舅舅2",
-        "orderid": 0,
+        "关系": "妻子",
+        "姓名": "崔丽",
+        "gender": "female",
         "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32170,
-        "mid": 32171,
+        "fid": null,
+        "mid": null,
         "pids": [
+            "1"
         ]
     },
     {
-        "id": 3,
-        "关系": "舅舅3",
-        "姓名": "舅舅4",
-        "orderid": 0,
+        "id": 1111111111,
+        "关系": "前妻",
+        "姓名": "崔丽",
+        "gender": "female",
         "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32170,
-        "mid": 32171,
+        "fid": null,
+        "mid": null,
         "pids": [
+            "1"
         ]
+    },
+
+    // 儿子
+    {
+        "id": 3,
+        "关系": "大儿子",
+        "姓名": "顾洋",
+        "gender": "male",
+        "详情": "(其他、不在册、非安置)",
+        "fid": 1,
+        "mid": 2,
+        "pids": null
     },
     {
         "id": 4,
-        "关系": "舅舅4",
-        "姓名": "舅舅4",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
+        "关系": "二儿子",
+        "姓名": "林刚",
         "gender": "male",
-        "fid": 32170,
-        "mid": 32171,
-        "pids": [
-        ]
+        "详情": "(其他、不在册、非安置)",
+        "fid": 1,
+        "mid": 2,
+        "pids": null
     },
     {
         "id": 5,
-        "关系": "舅舅5",
-        "姓名": "舅舅5",
-        "orderid": 0,
+        "关系": "三儿子",
+        "姓名": "郝超",
         "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
         "gender": "male",
-        "fid": 32170,
-        "mid": 32171,
-        "pids": [
-        ]
+        "fid": 1,
+        "mid": 2,
+        "pids": null
     },
-
+    // 前妻的儿子
     {
         "id": 6,
-        "关系": "大姨子",
-        "姓名": "大姨子",
-        "orderid": 0,
+        "关系": "前妻的儿子",
+        "姓名": "郝超",
         "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 32162,
-        "mid": 32163,
-        "pids": [
-            "7"
-        ]
-    },
-    {
-        "id": 7,
-        "关系": "大姨子丈夫",
-        "姓名": "大姨子丈夫",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
         "gender": "male",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "6"
-        ]
+        "fid": 1,
+        "mid": 1111111111,
+        "pids": null
     },
 
-    {
-        "id": 8,
-        "关系": "大姨子2",
-        "姓名": "大姨子2",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 32162,
-        "mid": 32163,
-        "pids": [
-            "9"
-        ]
-    },
-    {
-        "id": 9,
-        "关系": "大姨子丈夫2",
-        "姓名": "大姨子丈夫2",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "8"
-        ]
-    },
-
-    {
-        "id": 10,
-        "关系": "之子1",
-        "姓名": "之子1",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": [
-            "110"
-        ]
-    },
-    {
-        "id": 110,
-        "关系": "之子1的妻子",
-        "姓名": "之子1的妻子",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": null,
-        "mid": null,
-        "pids": [
-            "10"
-        ]
-    },
+    // 爸爸
     {
         "id": 11,
-        "关系": "之子2",
-        "姓名": "之子2",
-        "orderid": 0,
+        "关系": "父亲",
+        "姓名": "曹敏",
         "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
         "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": [
-            "111"
+        "fid": null,
+        "mid": null,
+        "pids":  [
+            "12"
         ]
     },
     {
-        "id": 111,
-        "关系": "之子2的妻子",
-        "姓名": "之子2的妻子",
-        "orderid": 0,
+        "id": 12,
+        "关系": "母亲",
+        "姓名": "程娜",
         "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
         "gender": "female",
         "fid": null,
         "mid": null,
         "pids": [
             "11"
-        ]
-    },
-    {
-        "id": 12,
-        "关系": "之子3",
-        "姓名": "之子3",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": [
-        ]
-    },
-    {
-        "id": 13,
-        "关系": "之子4",
-        "姓名": "之子4",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": [
-        ]
-    },
-    {
-        "id": 14,
-        "关系": "之子5",
-        "姓名": "之子5",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": [
-        ]
-    },
-    {
-        "id": 15,
-        "关系": "之子6",
-        "姓名": "之子6",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 32151,
-        "pids": [
-        ]
-    },
-    {
-        "id": 16,
-        "关系": "之子7",
-        "姓名": "之子7",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "male",
-        "fid": 32150,
-        "mid": 321511,
-        "pids": [
-        ]
-    },
-
-    {
-        "id": 17,
-        "关系": "小姨 1",
-        "姓名": "小姨1",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 321672,
-        "mid": 32173,
-        "pids": [
-        ]
-    },
-    {
-        "id": 18,
-        "关系": "小姨2",
-        "姓名": "小姨2",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 321672,
-        "mid": 32173,
-        "pids": [
-        ]
-    },
-    {
-        "id": 19,
-        "关系": "小姨3",
-        "姓名": "小姨3",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 32162,
-        "mid": 32163,
-        "pids": [
-        ]
-    },
-    {
-        "id": 20,
-        "关系": "小姨4",
-        "姓名": "小姨4",
-        "orderid": 0,
-        "详情": "(其他、不在册、非安置)",
-        "备注": null,
-        "头像": "/Content/Image/femail.png",
-        "gender": "female",
-        "fid": 32162,
-        "mid": 32163,
-        "pids": [
         ]
     },
 ]
